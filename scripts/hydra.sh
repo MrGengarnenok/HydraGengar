be47f840ad063d39cfc6acb85f43b9fffdae24f4
MrGengarnenok <140098355+MrGengarnenok@users.noreply.github.com>
2024-07-14 18:51:16 +0300

    Update hydra.sh

--- scripts/hydra.sh
+++ scripts/hydra.sh
@@ -53,14 +53,40 @@ discover_devices() {
     nmap -sn "$ip/24"
 }
 
-# ASCII-арт для заголовка
+# Функция для выполнения атаки на логин с помощью Hydra
+perform_hydra_attack() {
+    local target="$1"
+    local service="$2"
+    local username="$3"
+    local password_list="$4"
+    echo "Выполнение атаки на $target с помощью Hydra..."
+    hydra -l "$username" -P "$password_list" "$target" "$service"
+}
+
+# Функция для полного сканирования портов с nmap
+full_port_scan() {
+    local ip="$1"
+    echo "Полное сканирование портов для IP: $ip"
+    nmap -p- "$ip"
+}
+
+# Функция для получения информации об устройстве по IP
+get_device_info() {
+    local ip="$1"
+    echo "Получение информации об устройстве с IP: $ip"
+    nmap -A "$ip"
+}
+
+# ASCII-арт для заголовка с драконом и логотипом Kali Linux
 echo -e "\e[34m"
-echo "  _    _ _    _       _        _       "
-echo " | |  | | |  | |     | |      | |      "
-echo " | |__| | |  | | ___ | | _____| |_ ___ "
-echo " |  __  | |  | |/ _ \| |/ / _ \ __/ _ \ "
-echo " | |  | | |__| | (_) |   <  __/ ||  __/ "
-echo " |_|  |_|\____/ \___/|_|\_\___|\__\___| "
+echo "                    ___.-----.______.----.___"
+echo "                ===(o)==================(o)==="
+echo "                    '-----'        '-----'"
+echo "          ____            ____  _       _    _           _   "
+echo "         |  _ \ ___  __ _/ ___|| | ___ | |_ | |__   ___ | |_ "
+echo "         | |_) / _ \/ _' \___ \| |/ _ \| __|| '_ \ / _ \| __|"
+echo "         |  __/  __/ (_| |___) | | (_) | |_ | | | |  __/| |_ "
+echo "         |_|   \___|\__,_|____/|_|\___/ \__||_| |_|\___| \__|"
 echo -e "\e[0m"
 
 # Проверка обновлений перед показом меню
@@ -71,7 +97,10 @@ echo "Выберите действие:"
 echo "1) Проверить доступные сети"
 echo "2) Сканировать уязвимости"
 echo "3) Узнать устройства в сети"
-echo "4) Выйти"
+echo "4) Выполнить атаку с помощью Hydra"
+echo "5) Полное сканирование портов"
+echo "6) Получить информацию об устройстве"
+echo "7) Выйти"
 
 read -p "Введите номер вашего выбора: " choice
 
@@ -102,6 +131,21 @@ case $choice in
         discover_devices "$ip"
         ;;
     4)
+        read -p "Введите цель (IP или домен): " target
+        read -p "Введите сервис (например, http, ftp): " service
+        read -p "Введите имя пользователя: " username
+        read -p "Введите путь к файлу со списком паролей: " password_list
+        perform_hydra_attack "$target" "$service" "$username" "$password_list"
+        ;;
+    5)
+        ip=$(hostname -I | awk '{print $1}')
+        full_port_scan "$ip"
+        ;;
+    6)
+        read -p "Введите IP-адрес устройства: " device_ip
+        get_device_info "$device_ip"
+        ;;
+    7)
         echo "Выход из программы."
         exit 0
         ;;
